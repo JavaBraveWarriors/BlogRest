01bf8f5fb9bf709c997fd86e046af5ef5b7c053a
evtushenko-sasha <evtushenko-sasha@bk.ru>
2019-02-13 11:46:12 +0300

    Refactored Comment.

--- dao/src/main/java/com/blog/dao/jdbc/mapper/CommentRowMapper.java
+++ dao/src/main/java/com/blog/dao/jdbc/mapper/CommentRowMapper.java
@@ -18,16 +18,19 @@ public class CommentRowMapper implements RowMapper<Comment> {
     public static final String AUTHOR_ID = "comment_author_id";
     public static final String INITIAL = "initial";
     public static final String QUANTITY = "quantity";
+    public static final String AUTHOR_FIRST_NAME = "first_name";
+    public static final String AUTHOR_LAST_NAME = "last_name";
 
     @Override
     public Comment mapRow(ResultSet rs, int rowNum) throws SQLException {
         Comment comment = new Comment();
         comment.setId(rs.getLong(ID));
         comment.setText(rs.getString(TEXT));
+        comment.setAuthorFirstName(rs.getString(AUTHOR_FIRST_NAME));
+        comment.setAuthorLastName(rs.getString(AUTHOR_LAST_NAME));
         comment.setAuthorId(rs.getLong(AUTHOR_ID));
         comment.setPostId(rs.getLong(POST_ID));
         comment.setId(rs.getLong(ID));
-
         Timestamp date = rs.getTimestamp(CREATED_DATE);
         if (date != null) {
             comment.setTimeOfCreation(date.toLocalDateTime());
